af14947c47a9d5370614d1d8be97b5c1a7ae619d
salsowa <salsowa@bu.edu>
2025-01-28 18:48:22 -0500

    Update GEO assig.py

--- GEO assig.py
+++ GEO assig.py
@@ -1,4 +1,18 @@
 import math
+import tracemalloc
+import cProfile
+
+cProfile.run("20+10")
+tracemalloc.start()
+
+# ... run your application ...
+
+snapshot = tracemalloc.take_snapshot()
+top_stats = snapshot.statistics('lineno')
+
+print("[ Top 10 ]")
+for stat in top_stats[:10]:
+    print(stat)
 
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371
